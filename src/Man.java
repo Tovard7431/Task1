b2decb0a282eb7b7b2251e6851a83fe5c089b397
Домашний <khramovsam@gmail.com>
2025-05-28 23:18:28 +0300

    @Override  public boolean equals(Object obj) {};

--- src/Man.java
+++ src/Man.java
@@ -2,7 +2,7 @@ public class Man {
     private String name;
     private int age;
 
-    Man (String name, int age) {
+    Man(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -10,8 +10,23 @@ public class Man {
     public String getName() {
         return name;
     }
+
     public int getAge() {
         return age;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (!(obj instanceof Man))
+            return false;
+        Man man = (Man) obj;
+        if (age != man.age)
+            return false;
+        if (name != null ? !name.equals(man.name) : man.name != null)
+            return false;
+    }
+}
 
 }
