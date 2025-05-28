0bd739da860f37fd5509b9470f6d016c19145561
Домашний <khramovsam@gmail.com>
2025-05-28 23:02:45 +0300

    added get and set

--- src/Man.java
+++ src/Man.java
@@ -2,7 +2,7 @@ public class Man {
     private String name;
     private int age;
 
-    public Man (String name, int age) {
+    Man (String name, int age) {
         this.name = name;
         this.age = age;
     }
