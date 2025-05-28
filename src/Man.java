fc47f1acbb3123557c4321f8770376e3dec2e0d6
Домашний <khramovsam@gmail.com>
2025-05-28 23:00:18 +0300

    added get and set

--- src/Man.java
+++ src/Man.java
@@ -1,5 +1,17 @@
 public class Man {
     private String name;
     private int age;
-    //нужен геттер
+
+    public Man (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
 }
