a9f4efe208fea12b2b5f22714455631f09351c9b
sam <khamovsam@gmail.com>
2025-06-06 09:36:13 +0300

    Написал метод для сравнения двух програмистов, так же учёл прошлые коментарии.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,39 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Programmer programmer0 = new Programmer("Серёга", 25, 100);
-        Programmer programmer1 = new Programmer("Сэм", 25, 1);
-        Programmer programmer2 = new Programmer("Петя", 28, -100);
-        Programmer programmer3 = new Programmer("Сэм", 25, 1);
-        System.out.println(programmer0.equals(programmer1));
-        System.out.println(programmer1.equals(programmer0));
-        System.out.println(programmer2.equals(programmer2));
-        System.out.println(programmer1.equals(programmer3));
-        int programmer0sLvl = programmer0.getLvl();
-        int programmer1sLvl = programmer1.getLvl();
-        int programmer2sLvl = programmer2.getLvl();
-        if (programmer0sLvl < programmer1sLvl) {
-            System.out.println("Программист с именем " + programmer1.getName() + " и возрастом " + programmer1.getAge() + " круче программиста с именем " + programmer0.getName() + " и возрастом " + programmer0.getAge() + " на " + (programmer1sLvl - programmer0sLvl) + " уровней.");
+        Programmer p1 = new Programmer("Серёга", 25, 100);
+        Programmer p2 = new Programmer("Сэм", 25, 1);
+        Programmer p3 = new Programmer("Петя", 28, -100);
+        Programmer p4 = new Programmer("Сэм", 25, 1);
+        System.out.println(p1.equals(p2));
+        System.out.println(p2.equals(p1));
+        System.out.println(p3.equals(p3));
+        System.out.println(p2.equals(p4));
+        int programmer1sLvl = p1.getLvl();
+        int programmer2sLvl = p2.getLvl();
+        int programmer3sLvl = p3.getLvl();
+
+    }
+
+    public static void compareProgrammers(Programmer p1, Programmer p2) {
+        int lvlDifference = p1.getLvl() - p2.getLvl();
+        if (lvlDifference > 0) {
+            System.out.println("Программист с именем " + p1.getName() + " и возрастом " + p1.getAge() +
+                    " круче программиста с именем " + p2.getName() + " и возрастом " + p2.getAge() +
+                    " на " + lvlDifference + " уровней.");
+        } else if (lvlDifference < 0) {
+            System.out.println("Программист с именем " + p2.getName() + " и возрастом " + p2.getAge() +
+                    " круче программиста с именем " + p1.getName() + " и возрастом " + p1.getAge() +
+                    " на " + Math.abs(lvlDifference) + " уровней.");
         } else {
-            System.out.println("Программист с именем " + programmer0.getName() + " и возрастом " + programmer0.getAge() + " круче программиста с именем " + programmer1.getName() + " и возрастом " + programmer1.getAge() + " на " + (programmer0sLvl - programmer1sLvl) + " уровней.");
+            System.out.println("Программисты " + p1.getName() + " и " + p2.getName() + " находятся на одном уровне.");
         }
     }
 }
+
+
+/* if (programmer0sLvl > programmer1sLvl) {
+            System.out.println("Программист с именем " + p1.getName() + " и возрастом " + p1.getAge() + " круче программиста с именем " + p2.getName() + " и возрастом " + p2.getAge() + " на " + (programmer1sLvl - programmer2sLvl) + " уровней.");
+        } else {
+            System.out.println("Программист с именем " + p2.getName() + " и возрастом " + p2.getAge() + " круче программиста с именем " + p1.getName() + " и возрастом " + p1.getAge() + " на " + (programmer1sLvl - programmer1sLvl) + " уровней.");
+       } */
